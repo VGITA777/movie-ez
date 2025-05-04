ba4acdea11ae61b2e757b90fec6cb9a1dc0d06cc
Horizon <113239025+VGITA777@users.noreply.github.com>
2025-05-04 21:16:36 +0800

    Add Movies component with basic structure and routing
    Created the Movies component with HTML, SCSS, and TypeScript files. Implemented basic routing configuration for the Movies module, linking the component to its path. This sets up the foundation for further development of the Movies feature.

---
+++ src/app/movies/routing.ts
@@ -0,0 +1,9 @@
+import {Routes} from '@angular/router';
+import {MoviesComponent} from './movies.component';
+
+export const MoviesRoutes: Routes = [
+  {
+    path: '',
+    component: MoviesComponent
+  }
+];
