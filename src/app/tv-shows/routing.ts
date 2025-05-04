7dca40e01e49367d0c73995bec934cd939d4f23c
Horizon <113239025+VGITA777@users.noreply.github.com>
2025-05-04 21:14:43 +0800

    Add TV Shows component with routing setup
    Created a new `TvShowsComponent` with its HTML, SCSS, and TypeScript files. Configured routing for the component as the default path for its associated module.

---
+++ src/app/tv-shows/routing.ts
@@ -0,0 +1,9 @@
+import {Routes} from '@angular/router';
+import {TvShowsComponent} from './tv-shows.component';
+
+export const TvShowsRoutes: Routes = [
+  {
+    path: '',
+    component: TvShowsComponent
+  }
+];
