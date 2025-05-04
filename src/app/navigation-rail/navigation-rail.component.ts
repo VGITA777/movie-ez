e5e18bb2fc70f5c19ee3adbec663116e714fa045
Horizon <113239025+VGITA777@users.noreply.github.com>
2025-05-04 21:16:55 +0800

    Add navigation rail with Home, Movies, and TV sections
    Implemented a navigation rail component with entries for Home, Movies, and TV Shows, including active and inactive states. Integrated location listener and navigator services for dynamic state management, and added a basic SCSS layout for proper styling.

--- src/app/navigation-rail/navigation-rail.component.ts
+++ src/app/navigation-rail/navigation-rail.component.ts
@@ -1,11 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, Signal} from '@angular/core';
+import {NavigationRailEntryComponent} from './ui/navigation-entry/navigation-rail-entry.component';
+import {Page} from './utils/page';
+import {LocationListenerService} from '../shared/utils/location-listener.service';
+import {NavigatorService} from '../shared/utils/navigator.service';
 
 @Component({
   selector: 'app-navigation-rail',
-  imports: [],
+  imports: [
+    NavigationRailEntryComponent
+  ],
   templateUrl: './navigation-rail.component.html',
   styleUrl: './navigation-rail.component.scss'
 })
 export class NavigationRailComponent {
+  readonly currentLocation: Signal<Page>
+  protected readonly Page = Page;
+
+  constructor(readonly locationListener: LocationListenerService, readonly navigator: NavigatorService) {
+    this.currentLocation = locationListener.currentLocation;
+  }
 
 }
