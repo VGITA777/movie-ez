364c6bf3f9968cbb0e90c57ddea3a4aa52f79c5e
Horizon <113239025+VGITA777@users.noreply.github.com>
2025-05-05 21:05:46 +0800

    Created settings player page

---
+++ src/app/settings/features/settings-player/settings-player.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-settings-player',
+  imports: [],
+  templateUrl: './settings-player.component.html',
+  styleUrl: './settings-player.component.scss'
+})
+export class SettingsPlayerComponent {
+
+}
