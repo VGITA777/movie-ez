03bfb2bdc223dcd4bcb9ad844932d1469dd705ee
Horizon <113239025+VGITA777@users.noreply.github.com>
2025-05-05 13:49:03 +0800

    Add ToastService for centralized toast notifications
    This service provides methods to display various types of toast messages, such as success, error, info, warning, and more. It ensures centralized management of toast notifications using RxJS for state handling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavigationRailComponent} from './navigation-rail/navigation-rail.component';
+import {Toast} from 'primeng/toast';
+import {MessageService} from 'primeng/api';
+import {ToastService} from './shared/utils/toast.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationRailComponent],
+  imports: [RouterOutlet, NavigationRailComponent, Toast],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [MessageService]
 })
 export class AppComponent {
+
+  toastSubscription!: Subscription;
+
+  constructor(readonly messageService: MessageService, readonly toast: ToastService) {
+
+  }
+
+  ngOnInit() {
+    this.toastSubscription = this.toast.message.subscribe((e) => {
+      if (!e) {
+        return;
+      }
+      this.messageService.add(e);
+    })
+  }
+
+  ngOnDestroy() {
+    this.toastSubscription.unsubscribe();
+  }
 }
+
