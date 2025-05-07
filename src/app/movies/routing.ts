f6fbc13c8e4de6d1719b8ac372ec554435c53871
Horizon <113239025+VGITA777@users.noreply.github.com>
2025-05-07 21:56:24 +0800

    Moved resolver location

--- src/app/movies/routing.ts
+++ src/app/movies/routing.ts
@@ -1,6 +1,6 @@
 import {Routes} from '@angular/router';
 import {MoviesComponent} from './movies.component';
-import {PopularMoviesResolverService} from './data-access/popular-movies-resolver.service';
+import {PopularMoviesResolverService} from '../shared/data-access/popular-movies-resolver.service';
 import {NowPlayingMoviesService} from './data-access/now-playing-movies.service';
 import {TopRatedMoviesResolverService} from './data-access/top-rated-movies-resolver.service';
 import {LatestMoviesResolverService} from './data-access/latest-movies-resolver.service';
--- src/app/shared/data-access/popular-movies-resolver.service.ts
+++ src/app/shared/data-access/popular-movies-resolver.service.ts
@@ -1,9 +1,9 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, MaybeAsync, RedirectCommand, Resolve, RouterStateSnapshot} from '@angular/router';
 import {PopularMovies} from 'tmdb-ts';
-import {ProgressShowerService} from '../../shared/utils/progress-shower.service';
+import {ProgressShowerService} from '../utils/progress-shower.service';
 import {finalize, from, tap} from 'rxjs';
-import {TmdbService} from '../../shared/data-access/tmdb.service';
+import {TmdbService} from './tmdb.service';
 
 @Injectable({
   providedIn: 'root'
