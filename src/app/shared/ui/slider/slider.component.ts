32665d6c3bdb65cee89032573ffa044f618271a0
Horizon <113239025+VGITA777@users.noreply.github.com>
2025-05-13 23:40:29 +0800

    Updated slider.component.ts

--- src/app/shared/ui/slider/slider.component.ts
+++ src/app/shared/ui/slider/slider.component.ts
@@ -23,20 +23,21 @@ import {Pagination} from 'swiper/modules';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SliderComponent {
+  defaultSwiperOptions!: SwiperOptions;
+
   readonly design: InputSignal<TemplateRef<any>> = input.required();
   readonly data: InputSignal<any[]> = input.required();
   readonly gap: InputSignal<string> = input('unset');
   readonly swiperOptions: InputSignal<SwiperOptions | undefined> = input()
   readonly swiperContainer: Signal<ElementRef<SwiperContainer>> = viewChild.required('swiper')
-
   protected readonly swiperPagination: Signal<ElementRef<HTMLDivElement>> = viewChild.required('swiperPagination');
 
   ngOnInit() {
-    const defaultSwiperOptions: SwiperOptions = {
+    this.defaultSwiperOptions = {
       modules: [Pagination],
-      slidesPerView: 'auto',
+      slidesPerView: 8,
       direction: 'horizontal',
-      spaceBetween: 12,
+      spaceBetween: 8,
       pagination: {
         enabled: true,
         el: this.swiperPagination().nativeElement,
@@ -45,10 +46,46 @@ export class SliderComponent {
         renderBullet: (_index: number, className: string) => {
           return '<span class="' + className + '"></span>';
         }
+      },
+      breakpointsBase: 'container',
+      breakpoints: {
+        0: {
+          slidesPerView: 2.2,
+        },
+        375: {
+          slidesPerView: 2.5,
+        },
+        425: {
+          slidesPerView: 2.8,
+        },
+        475: {
+          slidesPerView: 3.5,
+        },
+        500: {
+          slidesPerView: 3.5,
+        },
+        600: {
+          slidesPerView: 4.5,
+        },
+        768: {
+          slidesPerView: 5.5,
+        },
+        1024: {
+          slidesPerView: 6.5,
+        },
+        1280: {
+          slidesPerView: 7.5,
+        },
+        1536: {
+          slidesPerView: 8.5,
+        },
+        1920: {
+          slidesPerView: 9.5,
+        }
       }
     }
     const sp: SwiperContainer = this.swiperContainer().nativeElement;
-    Object.assign(sp, this.swiperOptions() ?? defaultSwiperOptions);
+    Object.assign(sp, this.swiperOptions() ?? this.defaultSwiperOptions);
     sp.initialize();
   }
 }
