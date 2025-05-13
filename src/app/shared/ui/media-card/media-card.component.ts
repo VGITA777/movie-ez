7f49a8f50a54392d78e21f8cb60215e0c11db295
Horizon <113239025+VGITA777@users.noreply.github.com>
2025-05-14 00:21:28 +0800

    Add MediaCard component to UI library
    Introduce a reusable MediaCard component with HTML, SCSS, and TypeScript implementations. The component displays a media poster, title, type, and release date, styled with mixins and responsive design principles.

---
+++ src/app/shared/ui/media-card/media-card.component.ts
@@ -0,0 +1,17 @@
+import {Component, input, InputSignal} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-media-card',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './media-card.component.html',
+  styleUrl: './media-card.component.scss'
+})
+export class MediaCardComponent {
+  readonly title: InputSignal<string> = input.required();
+  readonly poster: InputSignal<string> = input.required();
+  readonly mediaType: InputSignal<string> = input.required();
+  readonly date: InputSignal<string> = input.required({transform: (date) => date.split('-')[0]});
+}
