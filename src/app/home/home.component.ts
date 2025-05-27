8cbc4c9681a523f7d9025695e1b460772dee6b5e
horizon <113239025+VGITA777@users.noreply.github.com>
2025-05-27 21:48:40 +0800

    Added back the sliders

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import {MovieDiscoverResult, PopularMovies, PopularTvShows, TvShowDiscoverResult
 import {take} from 'rxjs';
 import {ProgressShowerService} from '../shared/utils/progress-shower.service';
 import {MediaSliderComponent} from '../shared/ui/media-slider/media-slider.component';
+import {ShineCardComponent} from '../shared/ui/shine-card/shine-card.component';
 
 @Component({
   selector: 'app-home',
