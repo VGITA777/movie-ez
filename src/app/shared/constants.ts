cb99c4ba28359d36528d2fe6cdfc711f1f7bf516
horizon <113239025+VGITA777@users.noreply.github.com>
2025-05-28 13:14:22 +0800

    Add namespaces for movie and TV show cache keys
    Introduced separate namespace constants for improved organization and clarity of cache keys related to movies and TV shows. This enhances maintainability and makes cache key management more consistent across the app.

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -2,8 +2,26 @@ export const ONE_DAY_MILLIS = 1000 * 60 * 60 * 24;
 
 /* Movies Cache */
 export const MOVIES_CACHE_NAMESPACE = 'movies-cache';
+
+export const DISCOVER_MOVIES_NAMESPACE = 'discover-movies';
 export const DISCOVER_MOVIES_CACHE_KEY = 'discover-movies';
+
+export const LATEST_MOVIES_NAMESPACE = 'latest-movies';
 export const LATEST_MOVIES_CACHE_KEY = 'latest-movies';
+
+export const POPULAR_MOVIES_NAMESPACE = 'popular-movies';
 export const POPULAR_MOVIES_CACHE_KEY = 'popular-movies';
+
+export const TOP_RATED_MOVIES_NAMESPACE = 'top-rated-movies';
 export const TOP_RATED_MOVIES_CACHE_KEY = 'top-rated-movies';
+
+export const NOW_PLAYING_MOVIES_NAMESPACE = 'now-playing-movies';
 export const NOW_PLAYING_MOVIES_CACHE_KEY = 'now-playing-movies';
+
+/* TV shows Cache Keys */
+export const DISCOVER_TV_SHOWS_NAMESPACE = 'discover-tv-shows';
+export const DISCOVER_TV_SHOWS_CACHE_KEY = 'discover-tv-shows';
+
+export const POPULAR_TV_SHOWS_NAMESPACE = 'popular-tv-shows';
+export const POPULAR_TV_SHOWS_CACHE_KEY = 'popular-tv-shows';
+
