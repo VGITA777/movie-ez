bdc9af5f55b400e0099ca0082149ca1af6d09310
horizon <113239025+VGITA777@users.noreply.github.com>
2025-05-28 13:06:04 +0800

    Add CachedResolve class for reusable route data caching
    Introduce an abstract CachedResolve class to handle route data fetching with caching using a customizable CacheManager. This implementation supports error handling and optional post-processing through the `onFinish` hook. Aimed at improving performance and reducing redundant API calls.

---
+++ src/app/shared/caching/cached-resolve.ts
@@ -0,0 +1,33 @@
+import {ActivatedRouteSnapshot, MaybeAsync, RedirectCommand, Resolve, RouterStateSnapshot} from '@angular/router';
+import {catchError, from, tap} from 'rxjs';
+import {getOrFetchAndCache} from '../utils/helpers';
+import {ONE_DAY_MILLIS} from '../constants';
+import {CacheManager} from './cache-manager';
+import {SimpleCache} from './simple-cache';
+
+export abstract class CachedResolve<T, C extends SimpleCache> implements Resolve<T> {
+  protected constructor(private readonly cacheManager: CacheManager<C>, private readonly key: string) {
+  }
+
+  abstract fetch(): Promise<T>
+
+  onFinish?(err?: any): void;
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<RedirectCommand | T> {
+    return from(this.cacheManager.get(this.key)).pipe(
+      getOrFetchAndCache<T, C>(
+        this.cacheManager,
+        this.key,
+        () => this.fetch(),
+        ONE_DAY_MILLIS
+      ),
+      tap(() => {
+        this.onFinish?.()
+      }),
+      catchError(err => {
+        this.onFinish?.(err);
+        return Promise.reject(err);
+      })
+    )
+  }
+}
