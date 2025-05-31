a247c7dc3c3075119ca994e3080bd35393e106b1
princ <113239025+VGITA777@users.noreply.github.com>
2025-05-31 19:12:40 +0800

    Replace LatestMovies with UpcomingMovies in MoviesComponent, add MediaSliderComponent for displaying movie categories, and update styles for layout adjustments.

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,12 +1,15 @@
 import {Component, signal, WritableSignal} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {LatestMovie, MoviesPlayingNow, PopularMovies, TopRatedMovies} from 'tmdb-ts';
+import {MoviesPlayingNow, PopularMovies, TopRatedMovies, UpcomingMovies} from 'tmdb-ts';
 import {take} from 'rxjs';
 import {ProgressShowerService} from '../shared/utils/progress-shower.service';
+import {MediaSliderComponent} from '../shared/ui/media-slider/media-slider.component';
 
 @Component({
   selector: 'app-movies',
-  imports: [],
+  imports: [
+    MediaSliderComponent
+  ],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss'
 })
@@ -15,7 +18,7 @@ export class MoviesComponent {
   protected readonly popularMovies: WritableSignal<PopularMovies | undefined> = signal(undefined);
   protected readonly nowPlayingMovies: WritableSignal<MoviesPlayingNow | undefined> = signal(undefined);
   protected readonly topRatedMovies: WritableSignal<TopRatedMovies | undefined> = signal(undefined);
-  protected readonly latestMovies: WritableSignal<LatestMovie | undefined> = signal(undefined);
+  protected readonly upcomingMovies: WritableSignal<UpcomingMovies | undefined> = signal(undefined);
 
   constructor(readonly activatedRoute: ActivatedRoute, readonly progressShower: ProgressShowerService) {
   }
@@ -25,7 +28,7 @@ export class MoviesComponent {
       this.popularMovies.set(data['popular']);
       this.nowPlayingMovies.set(data['nowPlaying']);
       this.topRatedMovies.set(data['topRated']);
-      this.latestMovies.set(data['latest']);
+      this.upcomingMovies.set(data['upcoming']);
       this.progressShower.hide();
     })
   }
