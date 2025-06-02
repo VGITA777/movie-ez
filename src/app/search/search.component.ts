15ead3dd78c4aac77c3f0fe1666d2caaba13a876
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-02 14:37:51 +0800

    Implement TMDB API integration and enhance search functionality in `SearchComponent`

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,8 @@
-import {Component, signal, WritableSignal} from '@angular/core';
+import {Component, inject, model, ModelSignal, resource, ResourceRef, signal, WritableSignal} from '@angular/core';
 import {IconInputFieldComponent} from '../shared/ui/icon-input-field/icon-input-field.component';
 import {ShineCardComponent} from '../shared/ui/shine-card/shine-card.component';
+import {TmdbService} from '../shared/data-access/tmdb.service';
+import {MultiSearchResult, Search} from 'tmdb-ts';
 
 @Component({
   selector: 'app-search',
@@ -12,10 +14,22 @@ import {ShineCardComponent} from '../shared/ui/shine-card/shine-card.component';
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
-
-  readonly searchText: WritableSignal<string> = signal('');
+  readonly searchText: ModelSignal<string> = model('');
+  readonly debouncedSearchText: WritableSignal<string> = signal('');
+  private readonly tmdb: TmdbService = inject(TmdbService);
+  readonly searchResults: ResourceRef<Search<MultiSearchResult> | undefined> = resource({
+    defaultValue: {} as Search<MultiSearchResult>,
+    params: () => {
+      return {
+        query: this.debouncedSearchText()
+      }
+    },
+    loader: (resource) => this.tmdb.search.multi({
+      query: resource.params.query
+    })
+  })
 
   search(event: string) {
-    this.searchText.set(event);
+    this.debouncedSearchText.set(event);
   }
 }
