1b33d71a3e4709a4a69f6cdbb413e3dec2cdcd12
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-02 14:13:52 +0800

    Started to create the search page

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, signal, WritableSignal} from '@angular/core';
+import {IconInputFieldComponent} from '../shared/ui/icon-input-field/icon-input-field.component';
+import {ShineCardComponent} from '../shared/ui/shine-card/shine-card.component';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [
+    IconInputFieldComponent,
+    ShineCardComponent
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
 
+  readonly searchText: WritableSignal<string> = signal('');
+
+  search(event: string) {
+    this.searchText.set(event);
+  }
 }
