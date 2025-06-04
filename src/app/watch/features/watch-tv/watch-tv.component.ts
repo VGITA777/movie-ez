09d4e85168ee23d5ddf6dfd552126ff1963f4436
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-04 15:32:25 +0800

    Rename `WatchSeriesComponent` to `WatchTvComponent` and update associated files.

---
+++ src/app/watch/features/watch-tv/watch-tv.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-watch-tv',
+  imports: [],
+  templateUrl: './watch-tv.component.html',
+  styleUrl: './watch-tv.component.scss'
+})
+export class WatchTvComponent {
+
+}
