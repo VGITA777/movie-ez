c353806c5fd4cac1f2cfad4bf52d342091b1c3ee
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-05 14:33:53 +0800

    Enhance `WatchMovieComponent` to include sanitized `movieMediaWatchLink`, utilize `WritableSignal` for `movieWatchProvider`, and handle route parameters with `Signal`.

--- src/app/watch/features/watch-movie/watch-movie.component.ts
+++ src/app/watch/features/watch-movie/watch-movie.component.ts
@@ -1,4 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, computed, inject, Signal, signal, WritableSignal} from '@angular/core';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {MovieVidoraWatchProvider, MovieWatchProvider} from '../../../shared/watch-provider/watch-prover';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {map} from 'rxjs';
 
 @Component({
   selector: 'app-watch-movie',
@@ -7,5 +12,21 @@ import {Component} from '@angular/core';
   styleUrl: './watch-movie.component.scss'
 })
 export class WatchMovieComponent {
-
+  protected readonly activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  protected readonly domSanitizer: DomSanitizer = inject(DomSanitizer);
+  protected readonly movieWatchProvider: WritableSignal<MovieWatchProvider> = signal(new MovieVidoraWatchProvider())
+  protected readonly movieCurrentMediaInformation: Signal<{ id: string }> = toSignal(
+    this.activatedRoute.paramMap.pipe(
+      map((params: ParamMap) => {
+        return {
+          id: params.get('id') ?? '',
+        }
+      }),
+    ),
+    {initialValue: {} as { id: string }}
+  );
+  protected readonly movieMediaWatchLink: Signal<SafeResourceUrl> = computed(() => {
+    const mediaInformation = this.movieCurrentMediaInformation();
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(this.movieWatchProvider().provideLink(mediaInformation.id));
+  })
 }
