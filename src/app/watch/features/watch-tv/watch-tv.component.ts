e4e05cecbea8e851e737fe3d569f2203e0f59ad4
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-05 14:16:52 +0800

    Enhance `WatchTvComponent` to include sanitized `tvMediaWatchLink`, utilize `WritableSignal` for `tvWatchProvider`, and improve handling of route parameters.

--- src/app/watch/features/watch-tv/watch-tv.component.ts
+++ src/app/watch/features/watch-tv/watch-tv.component.ts
@@ -1,7 +1,9 @@
-import {Component, effect, inject} from '@angular/core';
+import {Component, computed, inject, signal, WritableSignal} from '@angular/core';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {map} from 'rxjs';
+import {TvVidoraWatchProvider, TvWatchProvider} from '../../../shared/watch-provider/watch-prover';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-watch-tv',
@@ -11,7 +13,9 @@ import {map} from 'rxjs';
 })
 export class WatchTvComponent {
   protected readonly activatedRoute: ActivatedRoute = inject(ActivatedRoute);
-  protected readonly tvMediaInformation = toSignal(
+  protected readonly domSanitizer = inject(DomSanitizer);
+  protected readonly tvWatchProvider: WritableSignal<TvWatchProvider> = signal(new TvVidoraWatchProvider())
+  protected readonly tvCurrentMediaInformation = toSignal(
     this.activatedRoute.paramMap.pipe(
       map((params: ParamMap) => {
         return {
@@ -19,16 +23,12 @@ export class WatchTvComponent {
           season: Number(params.get('season')) || 1,
           episode: Number(params.get('episode')) || 1
         }
-      })
+      }),
     ),
     {initialValue: {} as { id: string, season: number, episode: number }}
   );
-
-
-  constructor() {
-    effect(() => {
-      console.log(this.tvMediaInformation())
-    });
-  }
-
+  protected readonly tvMediaWatchLink = computed(() => {
+    const mediaInformation = this.tvCurrentMediaInformation();
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(this.tvWatchProvider().provideLink(mediaInformation.id, mediaInformation.season, mediaInformation.episode));
+  })
 }
