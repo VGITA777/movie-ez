393f85bd6506e17af057517fc864b14c86182851
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-05 11:49:49 +0800

    Refactor `WatchRoutes` to support nested routes and add outlets for `mediaWatch` and `mediaDescription`.

--- src/app/watch/watch.routes.ts
+++ src/app/watch/watch.routes.ts
@@ -1,17 +1,66 @@
 import {Routes} from '@angular/router';
 
 export const WatchRoutes: Routes = [
+  // Redirect to error if url is /watch
   {
     path: '',
-    redirectTo: '/',
-    pathMatch: 'full'
+    pathMatch: 'full',
+    redirectTo: '/error'
   },
   {
-    path: 'movie/:id',
-    loadComponent: () => import('./features/watch-movie/watch-movie.component').then(m => m.WatchMovieComponent)
+    path: '',
+    loadComponent: () => import('./watch.component').then(m => m.WatchComponent),
+    loadChildren: async (): Promise<Routes> => [
+      {
+        path: 'movie/:id',
+        loadChildren: () => MovieWatchRoutes
+      },
+      {
+        path: 'tv/:id',
+        loadChildren: () => TvWatchRoutes
+      }
+    ]
   },
+]
+
+export const MovieWatchRoutes: Routes = [
   {
-    path: 'tv/:id',
-    loadComponent: () => import('./features/watch-tv/watch-tv.component').then(m => m.WatchTvComponent)
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => [
+      {
+        path: '',
+        outlet: 'mediaWatch',
+        pathMatch: 'full',
+        loadComponent: () => import('./features/watch-movie/watch-movie.component').then(m => m.WatchMovieComponent)
+      },
+      {
+        path: '',
+        outlet: 'mediaDescription',
+        pathMatch: 'full',
+        loadComponent: () => import('./features/watch-movie-description/watch-movie-description.component').then(m => m.WatchMovieDescriptionComponent)
+      }
+    ]
+  }
+]
+
+export const TvWatchRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => [
+      {
+        path: '',
+        outlet: 'mediaWatch',
+        pathMatch: 'full',
+        loadComponent: () => import('./features/watch-tv/watch-tv.component').then(m => m.WatchTvComponent)
+      },
+      {
+        path: '',
+        outlet: 'mediaDescription',
+        pathMatch: 'full',
+        loadComponent: () => import('./features/watch-tv-description/watch-tv-description.component').then(m => m.WatchTvDescriptionComponent)
+      }
+    ]
   }
 ]
