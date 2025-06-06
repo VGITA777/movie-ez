fb061277af147ef052c1abeaa01a7c18d60c55a6
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-06 14:40:24 +0800

    Replace `PrimeNG` progress bar with `IndeterminateProgressBarComponent` and comment out unused `ToastService`.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,35 @@
 import {Component, inject, OnDestroy, OnInit, Signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavigationRailComponent} from './navigation-rail/navigation-rail.component';
-import {Toast} from 'primeng/toast';
-import {MessageService} from 'primeng/api';
-import {ToastService} from './shared/utils/toast.service';
 import {Subscription} from 'rxjs';
-import {ProgressBar} from 'primeng/progressbar';
 import {ProgressShowerService, ProgressType} from './shared/utils/progress-shower.service';
+import {
+  IndeterminateProgressBarComponent
+} from './shared/ui/indeterminate-progress-bar/indeterminate-progress-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationRailComponent, Toast, ProgressBar],
+  imports: [RouterOutlet, NavigationRailComponent, IndeterminateProgressBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  providers: [MessageService]
+  providers: []
 })
 export class AppComponent implements OnInit, OnDestroy {
 
   toastSubscription!: Subscription;
-  readonly messageService: MessageService = inject(MessageService);
-  readonly toast: ToastService = inject(ToastService);
+  /*  readonly messageService: MessageService = inject(MessageService);
+    readonly toast: ToastService = inject(ToastService);*/
   readonly progressShower: ProgressShowerService = inject(ProgressShowerService);
   readonly progress: Signal<number> = this.progressShower.progress;
   readonly progressType: Signal<ProgressType> = this.progressShower.progressType;
 
   ngOnInit() {
-    this.toastSubscription = this.toast.message.subscribe((e) => {
-      if (!e) {
-        return;
-      }
-      this.messageService.add(e);
-    })
+    /*    this.toastSubscription = this.toast.message.subscribe((e) => {
+          if (!e) {
+            return;
+          }
+          this.messageService.add(e);
+        })*/
   }
 
   ngOnDestroy() {
