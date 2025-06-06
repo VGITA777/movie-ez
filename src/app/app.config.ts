8bad56ca81490ab2b531be934bb0df837f8a59f0
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-06 14:21:50 +0800

    Comment out `providePrimeNG` as its usage is temporarily deprecated.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,21 +3,19 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {providePrimeNG} from 'primeng/config';
-import {MyTheme} from '../my-theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: MyTheme,
-        options: {
-          darkModeSelector: '.dark-mode',
-        }
-      },
-    })
+    /*    providePrimeNG({
+          theme: {
+            preset: MyTheme,
+            options: {
+              darkModeSelector: '.dark-mode',
+            }
+          },
+        })*/
   ]
 };
