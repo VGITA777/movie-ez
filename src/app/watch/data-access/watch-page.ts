dde80f56b5565f0ae02d71c830fc7422003bb6b6
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-07 15:09:23 +0800

    Extend `WatchPage` to support TV episodes with `TvMediaLinkProvider` and `TvShowGenericMediaInfo`.

--- src/app/watch/data-access/watch-page.ts
+++ src/app/watch/data-access/watch-page.ts
@@ -1,4 +1,4 @@
-import {MediaLinkProvider} from '../../shared/watch-provider/media-link-provider';
+import {MediaLinkProvider, TvMediaLinkProvider} from '../../shared/watch-provider/media-link-provider';
 import {MovieDetails, TvShowDetails} from 'tmdb-ts';
 import {computed, inject, resource, ResourceRef, Signal, WritableSignal} from '@angular/core';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
@@ -13,23 +13,28 @@ export interface TvShowGenericMediaInfo extends MovieGenericMediaInfo {
   episode: number
 }
 
-export abstract class WatchPage<P extends MediaLinkProvider, I extends MovieGenericMediaInfo, D extends MovieDetails | TvShowDetails> {
+export type GenericMediaInfo = MovieGenericMediaInfo | TvShowGenericMediaInfo;
+
+export abstract class WatchPage<P extends MediaLinkProvider, I extends GenericMediaInfo, D extends MovieDetails | TvShowDetails> {
   protected readonly domSanitizer: DomSanitizer = inject(DomSanitizer);
   protected readonly tmdb: TmdbService = inject(TmdbService);
 
   protected abstract readonly mediaLinkProvider: WritableSignal<P>;
   protected abstract readonly genericMediaInfo: Signal<I>;
-  protected readonly mediaResourceUrl: Signal<SafeResourceUrl> = computed(() => {
-    const mediaInformation = this.genericMediaInfo();
-    return this.domSanitizer.bypassSecurityTrustResourceUrl(this.mediaLinkProvider().provideLink(mediaInformation.id));
-  })
   protected readonly mediaDetails: ResourceRef<D> = resource({
     defaultValue: {} as D,
-    params: () => {
-      return {id: this.genericMediaInfo().id}
-    },
+    params: () => ({id: this.genericMediaInfo().id}),
     loader: (data) => this.loader(data.params.id)
-  })
+  });
+  protected readonly mediaResourceUrl: Signal<SafeResourceUrl> = computed(() => {
+    const mediaInformation = this.genericMediaInfo();
+    if ('season' in mediaInformation && 'episode' in mediaInformation && this.mediaLinkProvider() instanceof TvMediaLinkProvider) {
+      return this.domSanitizer.bypassSecurityTrustResourceUrl(
+        this.mediaLinkProvider().provideLink(mediaInformation.id, mediaInformation.season, mediaInformation.episode)
+      );
+    }
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(this.mediaLinkProvider().provideLink(mediaInformation.id));
+  });
 
   protected abstract loader(id: number): Promise<D>;
 }
