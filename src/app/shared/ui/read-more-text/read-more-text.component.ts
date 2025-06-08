c2d35e6c460813c75f104b5085d4f021f8eec7d5
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-08 16:59:33 +0800

    Created read more component

---
+++ src/app/shared/ui/read-more-text/read-more-text.component.ts
@@ -0,0 +1,29 @@
+import {Component, input, InputSignal, signal, WritableSignal} from '@angular/core';
+import {NgClass} from '@angular/common';
+
+@Component({
+  selector: 'app-read-more-text',
+  imports: [
+    NgClass
+  ],
+  templateUrl: './read-more-text.component.html',
+  styleUrl: './read-more-text.component.scss'
+})
+export class ReadMoreTextComponent {
+  readonly width: InputSignal<string> = input('unset');
+  readonly headerText: InputSignal<string> = input('');
+  readonly buttonText: InputSignal<string> = input("Read more");
+  readonly buttonTextExpanded: InputSignal<string> = input("Read less");
+  readonly contentLines: InputSignal<number> = input(3);
+  readonly contentTextAlign: InputSignal<string> = input('left');
+  readonly contentTextSize: InputSignal<string> = input('.9rem');
+  readonly contentLetterSpacing: InputSignal<string> = input('1px');
+  readonly contentFontWeight: InputSignal<string> = input('200');
+
+  protected readonly isExpanded: WritableSignal<boolean> = signal(false);
+
+  toggleExpand(): void {
+    this.isExpanded.update(value => !value);
+  }
+}
+
