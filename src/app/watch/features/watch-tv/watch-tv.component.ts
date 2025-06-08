6bbed0a51769e9aca06f6480e98e41a49f0ecd10
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-08 21:04:09 +0800

    Add `mediaRecommendations` with slider component and handle card click in `WatchTvComponent`

--- src/app/watch/features/watch-tv/watch-tv.component.ts
+++ src/app/watch/features/watch-tv/watch-tv.component.ts
@@ -1,31 +1,31 @@
 import {Component, computed, inject, linkedSignal, Signal, signal, WritableSignal} from '@angular/core';
 import {DropDownSelectComponent, Option} from '../../../shared/ui/drop-down-select/drop-down-select.component';
 import {TvShowGenericMediaInfo, WatchPage} from '../../data-access/watch-page';
-import {ActivatedRoute} from '@angular/router';
 import {TV_EMBED_OBJS, TvMediaLinkProvider} from '../../../shared/watch-provider/media-link-provider';
-import {toSignal} from '@angular/core/rxjs-interop';
-import {map} from 'rxjs';
-import {Season, TvShowDetails} from 'tmdb-ts';
+import {Recommendations, Season, TvShowDetails} from 'tmdb-ts';
 import {VideoSource} from '../../../shared/constants';
 import {ShineCardComponent} from '../../../shared/ui/shine-card/shine-card.component';
+import {MediaLike, MediaSliderComponent} from '../../../shared/ui/media-slider/media-slider.component';
+import {NavigatorService} from '../../../shared/utils/navigator.service';
 
 @Component({
   selector: 'app-watch-tv',
   imports: [
     DropDownSelectComponent,
-    ShineCardComponent
+    ShineCardComponent,
+    MediaSliderComponent
   ],
   templateUrl: './watch-tv.component.html',
   styleUrl: './watch-tv.component.scss'
 })
 export class WatchTvComponent extends WatchPage<TvMediaLinkProvider, TvShowGenericMediaInfo, TvShowDetails> {
-  protected readonly activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  protected readonly navigatorService: NavigatorService = inject(NavigatorService);
   protected readonly mediaLinkProviders: Signal<Record<VideoSource, TvMediaLinkProvider>> = signal(TV_EMBED_OBJS);
-  protected readonly mediaId: Signal<number> = toSignal(this.activatedRoute.paramMap.pipe(map((params) => Number(params.get('id')) ?? 0)), {initialValue: 0});
   // Season Options
   protected readonly seasons: Signal<Season[]> = computed(() => this.mediaDetails
-    .value().seasons
-    .filter(s => s.season_number !== 0) ?? []);
+    .value()?.seasons
+    ?.filter(s => s.season_number !== 0) ?? []);
   // Selections
   protected readonly selectedSeasonOption: WritableSignal<Option> = signal({label: '1', value: 1});
   protected readonly seasonsOptions: Signal<Option[]> = computed((): Option[] =>
@@ -45,13 +45,23 @@ export class WatchTvComponent extends WatchPage<TvMediaLinkProvider, TvShowGener
     source: this.selectedSeasonOption,
     computation: (): Option => ({label: '1', value: 1}),
   })
+
+  // Used for the generic media info (ID, Season, Episode)
   protected override readonly genericMediaInfo: Signal<TvShowGenericMediaInfo> = computed(() => ({
     id: this.mediaId(),
     season: Number(this.selectedSeasonOption().value) ?? 1,
     episode: Number(this.selectedEpisodeOption().value) ?? 1
   }));
 
-  protected override loader(id: number): Promise<TvShowDetails> {
+  protected override mediaDetailsLoader(id: number): Promise<TvShowDetails> {
     return this.tmdb.tvShows.details(id);
+  }
+
+  protected override mediaRecommendationsLoader(id: number): Promise<Recommendations> {
+    return this.tmdb.tvShows.recommendations(id);
+  }
+
+  protected handleOnCardClick(media: MediaLike): void {
+    this.navigatorService.navigateToWatch(media);
   }
 }
