bda9551af895796786c71848604e861cbb8fba53
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-08 21:16:02 +0800

    Refactor `WatchMovieComponent` to add support for media recommendations and rename loader method for clarity

--- src/app/watch/features/watch-movie/watch-movie.component.ts
+++ src/app/watch/features/watch-movie/watch-movie.component.ts
@@ -1,8 +1,7 @@
-import {Component, inject, signal, Signal} from '@angular/core';
+import {Component, signal, Signal} from '@angular/core';
 import {MOVIE_EMBED_OBJS, MovieMediaLinkProvider,} from '../../../shared/watch-provider/media-link-provider';
-import {MovieDetails} from 'tmdb-ts';
+import {MovieDetails, Recommendations} from 'tmdb-ts';
 import {MovieGenericMediaInfo, WatchPage} from '../../data-access/watch-page';
-import {ActivatedRoute} from '@angular/router';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {map} from 'rxjs';
 import {VideoSource} from '../../../shared/constants';
@@ -17,14 +16,18 @@ import {DropDownSelectComponent} from '../../../shared/ui/drop-down-select/drop-
   styleUrl: './watch-movie.component.scss'
 })
 export class WatchMovieComponent extends WatchPage<MovieMediaLinkProvider, MovieGenericMediaInfo, MovieDetails> {
-  protected readonly activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+
   protected override mediaLinkProviders: Signal<Record<VideoSource, MovieMediaLinkProvider>> = signal(MOVIE_EMBED_OBJS);
   protected override readonly genericMediaInfo: Signal<MovieGenericMediaInfo> = toSignal(
     this.activatedRoute.paramMap.pipe(map((params): MovieGenericMediaInfo => ({id: Number(params.get('id')) ?? 0}))),
     {initialValue: {} as MovieGenericMediaInfo}
   )
 
-  protected override loader(id: number): Promise<MovieDetails> {
+  protected override mediaDetailsLoader(id: number): Promise<MovieDetails> {
     return this.tmdb.movies.details(id);
+  }
+
+  protected override mediaRecommendationsLoader(id: number): Promise<Recommendations> {
+    return this.tmdb.movies.recommendations(id);
   }
 }
