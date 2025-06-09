ab86513c5f967e4eafc5e50ff71e05c241c1a76b
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-09 17:07:41 +0800

    Add `defaultImageSize` and utility `fullImageUrl` in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
   isLoggingEnabled: false,
   apiKey: '',
   imageBaseUrl: "https://image.tmdb.org/t/p/",
+  defaultImageSize: "w500",
+  fullImageUrl: (path: string, size: string) => `https://image.tmdb.org/t/p/${size}${path}`,
   language: "en-US"
 };
