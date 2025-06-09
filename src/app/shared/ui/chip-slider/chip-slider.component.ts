3b2c9f347a44fd583524c03aa8f89076ee366d32
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-09 18:14:13 +0800

    Add `withShine` input to `ChipSliderComponent` for conditional chip styling with `ShineCardComponent` integration

--- src/app/shared/ui/chip-slider/chip-slider.component.ts
+++ src/app/shared/ui/chip-slider/chip-slider.component.ts
@@ -1,18 +1,23 @@
 import {Component, input, InputSignal} from '@angular/core';
 import {SliderComponent} from '../slider/slider.component';
+import {ShineCardComponent} from '../shine-card/shine-card.component';
+import {NgTemplateOutlet} from '@angular/common';
 
 @Component({
   selector: 'app-chip-slider',
   imports: [
-    SliderComponent
+    SliderComponent,
+    ShineCardComponent,
+    NgTemplateOutlet
   ],
   templateUrl: './chip-slider.component.html',
   styleUrl: './chip-slider.component.scss'
 })
 export class ChipSliderComponent {
   readonly data: InputSignal<string[]> = input.required();
-  readonly fontSize: InputSignal<string> = input('.8rem');
+  readonly fontSize: InputSignal<string> = input('.7rem');
   readonly chipBackgroundColor: InputSignal<string> = input('unset');
   readonly chipTextColor: InputSignal<string> = input('unset');
   readonly width: InputSignal<string> = input('100%');
+  readonly withShine: InputSignal<boolean> = input(true);
 }
