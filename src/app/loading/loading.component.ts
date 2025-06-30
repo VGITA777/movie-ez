420c0faf921b72398686544fcf9403e07226a9a3
princ <113239025+VGITA777@users.noreply.github.com>
2025-06-30 15:14:29 +0800

    Animate loading text per character with staggered effect, update styles and HTML structure

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,8 +1,11 @@
 import {Component, signal, WritableSignal} from '@angular/core';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-loading',
-  imports: [],
+  imports: [
+    NgStyle
+  ],
   templateUrl: './loading.component.html',
   styleUrl: './loading.component.scss'
 })
