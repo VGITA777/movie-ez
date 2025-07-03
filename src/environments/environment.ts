ab0e57698a130b6e63a9f042062127e000120bcf
princ <113239025+VGITA777@users.noreply.github.com>
2025-07-03 13:56:20 +0800

    Update environment configuration: modify language setting and adjust fullImageUrl function

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   isLoggingEnabled: false,
   apiKey: '',
   imageBaseUrl: "https://image.tmdb.org/t/p/",
+  fullImageUrl: (path: string, size: string) => `https://image.tmdb.org/t/p/${size}${path}`,
+  language: "en",
   defaultImageSize: "w500",
   defaultWatchImageSize: "original",
-  fullImageUrl: (path: string, size: string) => `https://image.tmdb.org/t/p/${size}${path}`,
-  language: "en-US"
 };
