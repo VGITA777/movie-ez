f8b8b18f9e5d45070bfebc3419e3a07d0a6dba86
princ <113239025+VGITA777@users.noreply.github.com>
2025-07-05 02:07:00 +0800

    Refactor cache handling: improve expiration check logic and enhance error logging for cached values

--- src/app/shared/utils/helpers.ts
+++ src/app/shared/utils/helpers.ts
@@ -9,14 +9,23 @@ export function getOrFetchAndCache<T, C extends SimpleCache<T>>(
   expiration: number
 ) {
   return mergeMap((cached: C | undefined) => {
-    if (cached && cached?.expiration !== undefined && cached?.expiration > Date.now()) {
-      try {
-        const value = typeof cached.value === 'string' ? JSON.parse(cached.value) : cached.value;
+    if (cached) {
+      const value = typeof cached.value === 'string' ? JSON.parse(cached.value) : cached.value;
+      // No expiration means the cache is always valid
+      if (cached.expiration === undefined) {
+        console.log(`Returning value from cache: ${cacheKey}`)
         return of(value as T);
-      } catch (e) {
-        cacheManager.clear()
-        console.log(`Error parsing cached value for key ${cacheKey}:`, e);
-        console.log(`Cached value:`, cached.value);
+      }
+      // Check if the cached value is still valid
+      if (cached?.expiration > Date.now()) {
+        try {
+          console.log(`Returning value from cache: ${cacheKey}`)
+          return of(value as T);
+        } catch (e) {
+          cacheManager.clear()
+          console.log(`Error parsing cached value for key ${cacheKey}:`, e);
+          console.log(`Cached value:`, cached.value);
+        }
       }
     }
     return from(fetchFn()).pipe(
