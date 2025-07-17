416cfcd24411f18fbea9522ee6d87575a04b8ab2
VGITA777 <113239025+VGITA777@users.noreply.github.com>
2025-07-18 03:26:13 +0800

    Update Angular dependencies to version 20.1.1 and add allowedCommonJsDependencies for tmdb-ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
+/*
+ * Copyright (c) 2025. This code is created by Prince Angelo Coquia.
+ */
+
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideAnimationsAsync(),
     /*    providePrimeNG({
           theme: {
             preset: MyTheme,
