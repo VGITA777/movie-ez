b063947785f07cab128263cf1f0c538e7d6dee76
horizon <113239025+VGITA777@users.noreply.github.com>
2025-09-16 14:24:58 +0800

    Moved getOrFetchAndCache function to the same file and changed mergeMap to concatMap

--- src/app/shared/caching/cached-resolve.ts
+++ src/app/shared/caching/cached-resolve.ts
@@ -1,9 +1,9 @@
 import {ActivatedRouteSnapshot, MaybeAsync, RedirectCommand, Resolve, RouterStateSnapshot} from '@angular/router';
-import {catchError, from, tap} from 'rxjs';
-import {getOrFetchAndCache} from '../utils/helpers';
-import {ONE_DAY_MILLIS} from '../constants';
+import {catchError, concatMap, from, of, take, tap} from 'rxjs';
+import {ONE_DAY_MILLIS} from '@constants';
 import {CacheManager} from './cache-manager';
 import {SimpleCache} from './simple-cache';
+import {environment} from '@env/environment';
 
 export abstract class CachedResolve<T, C extends SimpleCache<T>> implements Resolve<T> {
   protected constructor(private readonly cacheManager: CacheManager<C>, private readonly key: string) {
@@ -30,4 +30,42 @@ export abstract class CachedResolve<T, C extends SimpleCache<T>> implements Reso
       })
     )
   }
+}
+
+function getOrFetchAndCache<T, C extends SimpleCache<T>>(
+  cacheManager: CacheManager<C>,
+  cacheKey: string,
+  fetchFn: () => Promise<T>,
+  expiration: number
+) {
+  return concatMap((cached: C | undefined) => {
+    if (cached) {
+      const value = typeof cached.value === 'string' ? JSON.parse(cached.value) : cached.value;
+      // No expiration means the cache is always valid
+      if (cached.expiration === undefined) {
+        if (environment.isLoggingEnabled) {
+          console.log(`Returning value from cache: ${cacheKey}`)
+        }
+        return of(value as T);
+      }
+      // Check if the cached value is still valid
+      if (cached?.expiration > Date.now()) {
+        try {
+          if (environment.isLoggingEnabled) {
+            console.log(`Returning value from cache: ${cacheKey}`)
+          }
+          return of(value as T);
+        } catch (e) {
+          cacheManager.clear()
+          if (environment.isLoggingEnabled) {
+            console.error(`Error parsing cached value for key ${cacheKey}:`, e);
+          }
+        }
+      }
+    }
+    return from(fetchFn()).pipe(
+      take(1),
+      tap(result => cacheManager.set(cacheKey, result, expiration))
+    );
+  });
 }
