d635cbf7649caabf4da26e4c7871f6e514b5625a
VGITA777 <113239025+VGITA777@users.noreply.github.com>
2026-04-20 22:40:57 +0800

    Renamed application and added .gitignore

--- backend/src/main/java/dev/prince/movieez/MovieEzApplication.java
+++ backend/src/main/java/dev/prince/movieez/MovieEzApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class MovieEzApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(BackendApplication.class, args);
+  static void main(String[] args) {
+    SpringApplication.run(MovieEzApplication.class, args);
   }
 
 }
