69566032133fc66e5eee7728b7a3b505be9ca4bb
horizon <113239025+VGITA777@users.noreply.github.com>
2026-04-25 22:16:14 +0800

    Started to create the slider

--- frontend/src/app/home/ui/hero-slider/hero-slider.me.ts
+++ frontend/src/app/home/ui/hero-slider/hero-slider.me.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, input, InputSignal } from '@angular/core';
 import { HlmCarouselImports } from '@spartan-ng/helm/carousel';
+import { HlmCardImports } from '@spartan-ng/helm/card';
+import { NgOptimizedImage } from '@angular/common';
+import { MediaType } from '@shared/models';
+
+export interface HomeHeroSliderItem {
+  readonly imgSrc: string;
+  readonly title: string;
+  readonly rating: number;
+  readonly type: MediaType;
+}
 
 @Component({
   selector: 'me-hero-slider',
-  imports: [HlmCarouselImports],
+  imports: [HlmCarouselImports, HlmCardImports, NgOptimizedImage],
   templateUrl: './hero-slider.me.html',
   styleUrl: './hero-slider.me.css',
   host: {
     width: '100%',
   },
 })
-export class HeroSliderMe {}
+export class HeroSliderMe {
+  public readonly items: InputSignal<HomeHeroSliderItem[]> = input.required();
+}
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  tmdb: {
+    apiKey: '',
+    imageBaseUrl: 'https://image.tmdb.org/t/p/',
+  },
+};
