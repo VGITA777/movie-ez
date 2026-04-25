ef3b7ad60ae96952a222db8e875d756e17f9ad2f
horizon <113239025+VGITA777@users.noreply.github.com>
2026-04-26 01:50:30 +0800

    Some updates with responsiveness

--- frontend/src/app/home/home.me.ts
+++ frontend/src/app/home/home.me.ts
@@ -12,11 +12,13 @@ export class HomeMe {
   protected readonly items: HomeHeroSliderItem[] = [
     {
       imgSrc: `${environment.tmdb.imageBaseUrl}original/2ssWTSVklAEc98frZUQhgtGHx7s.jpg`,
-      title: 'Interstellar',
+      title: 'A very long title that cannot fit in one line.',
       rating: 9.8,
       type: 'movie',
       description:
-        "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival. " +
+        "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival. " +
+        "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival",
       year: 2024,
       tag: 'Sci-Fi / Adventure',
     },
