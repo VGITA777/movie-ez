f2d148a597870211314447220e5f4de81dfe5cbd
horizon <113239025+VGITA777@users.noreply.github.com>
2026-04-27 23:52:12 +0800

    Updated the media-carousel-backdrop-item

--- frontend/src/app/shared/ui/media-carousel/media-carousel-backdrop-item/media-carousel-backdrop-item.me.ts
+++ frontend/src/app/shared/ui/media-carousel/media-carousel-backdrop-item/media-carousel-backdrop-item.me.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, input, InputSignal } from '@angular/core';
+import { MediaCarouselItem } from '@shared/ui/media-carousel/media-carousel.me';
+import { NgOptimizedImage } from '@angular/common';
+import { HlmIconImports } from '@spartan-ng/helm/icon';
+import { provideIcons } from '@ng-icons/core';
+import { lucideClock } from '@ng-icons/lucide';
+
+export interface MediaCarouselBackdropItem extends MediaCarouselItem {
+  readonly runtime: number;
+}
 
 @Component({
   selector: 'me-media-carousel-backdrop-item',
-  imports: [],
+  imports: [NgOptimizedImage, HlmIconImports],
   templateUrl: './media-carousel-backdrop-item.me.html',
   styleUrl: './media-carousel-backdrop-item.me.css',
+  providers: [provideIcons({ lucideClock })],
 })
-export class MediaCarouselBackdropItemMe {}
+export class MediaCarouselBackdropItemMe {
+  public readonly item: InputSignal<MediaCarouselBackdropItem> = input.required();
+
+  protected convertRuntimeToHoursAndMinutes(runtime: number): string {
+    const hours: number = Math.floor(runtime / 60);
+    const minutes: number = runtime % 60;
+    return `${hours}h ${minutes.toFixed(0)}m`;
+  }
+}
