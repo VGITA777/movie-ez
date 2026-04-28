bfc03bccc28c07de2683ac76beab2c44455be79d
horizon <113239025+VGITA777@users.noreply.github.com>
2026-04-28 22:00:37 +0800

    Added a toast that shows the user that they can swipe the carousels. Additionally added signality

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmToasterImports } from '@spartan-ng/helm/sonner';
+import { toast } from '@spartan-ng/brain/sonner';
+import { storage } from '@signality/core';
 
 @Component({
   selector: 'me-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmToasterImports],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements AfterViewInit {
+  private readonly showSwipeToast: WritableSignal<boolean> = storage('showSwipeToast', true);
+
+  ngAfterViewInit(): void {
+    this.triggerSwipeToast();
+  }
+
+  private triggerSwipeToast(): void {
+    if (!this.showSwipeToast()) {
+      return;
+    }
+    toast.info('Did you know? The sliders can be swiped!', {
+      position: 'top-right',
+      duration: 8000,
+      action: {
+        label: 'Ok!',
+        onClick: () => {
+          this.showSwipeToast.set(false);
+        },
+      },
+    });
+  }
 }
