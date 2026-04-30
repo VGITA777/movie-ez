1c84c5a72189fc2720a76e0b310a15fc08e46357
horizon <113239025+VGITA777@users.noreply.github.com>
2026-04-30 21:59:55 +0800

    Updates on jackson

--- backend/src/main/java/dev/prince/movieez/media/tmdb/services/AppClientService.java
+++ backend/src/main/java/dev/prince/movieez/media/tmdb/services/AppClientService.java
@@ -2,10 +2,12 @@ package dev.prince.movieez.media.tmdb.services;
 
 import dev.prince.movieez.media.properties.TMDBSettings;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import tools.jackson.databind.json.JsonMapper;
 
 @Service
 public class AppClientService {
@@ -14,13 +16,21 @@ public class AppClientService {
   private final RestClientAdapter restClientAdapter;
   private final HttpServiceProxyFactory httpServiceProxyFactory;
 
-  public AppClientService(TMDBSettings tmdbSettings) {
-    this.restClient = RestClient.builder()
-                                .baseUrl(tmdbSettings.apiBaseUrl())
-                                .defaultHeader("Authorization", "Bearer " + tmdbSettings.apiKey())
-                                .build();
+  public AppClientService(TMDBSettings tmdbSettings, JsonMapper jsonMapper) {
+    this.restClient = RestClient
+        .builder()
+        .baseUrl(tmdbSettings.apiBaseUrl())
+        .configureMessageConverters(converters -> {
+          converters.configureMessageConvertersList(httpMessageConverters -> {
+            httpMessageConverters.add(new JacksonJsonHttpMessageConverter(jsonMapper));
+          });
+        })
+        .defaultHeader("Authorization", "Bearer " + tmdbSettings.apiKey())
+        .build();
     this.restClientAdapter = RestClientAdapter.create(restClient);
-    this.httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+    this.httpServiceProxyFactory = HttpServiceProxyFactory
+        .builderFor(restClientAdapter)
+        .build();
   }
 
   @Bean
