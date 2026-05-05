5c845268abc52624e74663482cbd95f0c853ae5c
horizon <113239025+VGITA777@users.noreply.github.com>
2026-05-05 15:48:14 +0800

    Updates with the search

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
 import { homeRoutes } from '@home/home.me.routes';
-import { watchRoutes } from './watch/watch.me.routes';
+import { watchRoutes } from '@watch/watch.me.routes';
+import { SearchMe } from '@search/search.me';
 
-export const routes: Routes = [...homeRoutes, ...watchRoutes];
+export const routes: Routes = [
+  ...homeRoutes,
+  ...watchRoutes,
+  {
+    path: 'search',
+    component: SearchMe,
+  },
+];
--- frontend/src/app/search/search.me.ts
+++ frontend/src/app/search/search.me.ts
@@ -1,9 +1,63 @@
 import { Component } from '@angular/core';
+import { HlmInputGroupImports } from '@spartan-ng/helm/input-group';
+import { HlmIconImports } from '@spartan-ng/helm/icon';
+import { provideIcons } from '@ng-icons/core';
+import { lucideSearch } from '@ng-icons/lucide';
+import { HlmToggleGroupImports } from '@spartan-ng/helm/toggle-group';
+import { MediaCarouselCoverItemMe } from '@shared/ui/media-carousel/media-carousel-cover-item/media-carousel-cover-item.me';
+import { MediaCarouselItem } from '@shared/ui/media-carousel/media-carousel.me';
+import { environment } from '@environments/environment';
+import { NgTemplateOutlet } from '@angular/common';
+import { HlmSeparatorImports } from '@spartan-ng/helm/separator';
 
 @Component({
   selector: 'me-search',
-  imports: [],
+  imports: [
+    HlmInputGroupImports,
+    HlmIconImports,
+    HlmToggleGroupImports,
+    MediaCarouselCoverItemMe,
+    NgTemplateOutlet,
+    HlmSeparatorImports,
+  ],
   templateUrl: './search.me.html',
   styleUrl: './search.me.css',
+  providers: [provideIcons({ lucideSearch })],
 })
-export class SearchMe {}
+export class SearchMe {
+  protected readonly items: MediaCarouselItem[] = [
+    {
+      id: 157336,
+      title: 'Interstellar',
+      imgSrc: `${environment.tmdb.imageBaseUrl}original/yQvGrMoipbRoddT0ZR8tPoR7NfX.jpg`,
+      rating: 9.8,
+      genres: ['Sci-Fi'],
+      year: 2024,
+      videoSrc:
+        'https://www.youtube.com/embed/zSWdZVtXT7E?autoplay=1&mute=1&controls=0&loop=1&disablekb=1?playlist=zSWdZVtXT7E',
+      type: 'movie',
+    },
+    {
+      id: 1290417,
+      title: 'Thrash',
+      imgSrc: `${environment.tmdb.imageBaseUrl}original/adk8weka3O5648g3de4z3y4aE7G.jpg`,
+      rating: 6.8,
+      genres: ['Adventure'],
+      year: 2026,
+      videoSrc:
+        'https://www.youtube.com/embed/hzyOsNyDkbM?autoplay=1&mute=1&controls=0&loop=1&disablekb=1?playlist=hzyOsNyDkbM',
+      type: 'movie',
+    },
+    {
+      id: 76479,
+      title: 'The Boys',
+      imgSrc: `${environment.tmdb.imageBaseUrl}original/fRNBdaCZMM3DPGTtqdg6Zf1XwX5.jpg`,
+      rating: 6.8,
+      genres: ['Action', 'Crime'],
+      year: 2024,
+      videoSrc:
+        'https://www.youtube.com/embed/tcrNsIaQkb4?autoplay=1&mute=1&controls=0&loop=1&disablekb=1?playlist=tcrNsIaQkb4',
+      type: 'tv',
+    },
+  ];
+}
