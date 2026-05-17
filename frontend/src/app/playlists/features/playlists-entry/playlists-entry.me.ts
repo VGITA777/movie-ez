6c4cb88e141bea670274ec8303f028ace565875c
horizon <113239025+VGITA777@users.noreply.github.com>
2026-05-18 00:14:16 +0800

    Some updates with the playlists page

--- frontend/src/app/playlists/features/playlists-entry/playlists-entry.me.ts
+++ frontend/src/app/playlists/features/playlists-entry/playlists-entry.me.ts
@@ -1,4 +1,12 @@
-import { Component, inject, input, InputSignal, ResourceRef } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  InputSignal,
+  ResourceRef,
+  Signal,
+} from '@angular/core';
 import {
   ImagesModel,
   MediaType,
@@ -10,25 +18,29 @@ import { MediaMovieService } from '@shared/services/media/media-movie.service';
 import { MediaTvSeriesService } from '@shared/services/media/media-tv-series-series.service';
 import { first, forkJoin, map, Observable } from 'rxjs';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { NgOptimizedImage } from '@angular/common';
+import { environment } from '@environments/environment';
 
-type PlaylistItemWithImages = OfflinePlaylistContent & {
+type PlaylistItemWithImagesAndDetails = OfflinePlaylistContent & {
   images: ImagesModel;
 };
 
 @Component({
   selector: 'me-playlists-entry',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './playlists-entry.me.html',
   styleUrl: './playlists-entry.me.css',
 })
 export class PlaylistsEntryMe {
-  public readonly data: InputSignal<OfflinePlaylist> = input.required();
+  private readonly mediaMovieService: MediaMovieService = inject(MediaMovieService);
+  private readonly mediaTvSeriesService: MediaTvSeriesService = inject(MediaTvSeriesService);
 
-  protected readonly images: ResourceRef<PlaylistItemWithImages[]> = rxResource({
+  protected readonly images: ResourceRef<PlaylistItemWithImagesAndDetails[]> = rxResource({
     defaultValue: [],
-    params: (): OfflinePlaylistContent[] => this.data().items,
-    stream: (data): Observable<PlaylistItemWithImages[]> => {
+    params: (): OfflinePlaylistContent[] => this.playlist().items,
+    stream: (data): Observable<PlaylistItemWithImagesAndDetails[]> => {
       const requests$ = data.params
+        .slice(0, 4) // Limit to 4 items.
         .map((item) => {
           const trackId: number = parseInt(item.trackId, 10);
           const mediaType: SearchableMediaType = item.mediaType as SearchableMediaType;
@@ -40,21 +52,90 @@ export class PlaylistsEntryMe {
           return this.getMediaImages(trackId, mediaType).pipe(
             first(),
             map(
-              (images): PlaylistItemWithImages => ({
+              (images): PlaylistItemWithImagesAndDetails => ({
                 ...item,
                 images: images,
               }),
             ),
           );
         })
-        .filter((request): request is Observable<PlaylistItemWithImages> => request !== undefined);
+        .filter(
+          (request): request is Observable<PlaylistItemWithImagesAndDetails> =>
+            request !== undefined,
+        );
 
       return forkJoin(requests$);
     },
   });
+  protected readonly singleImage: Signal<string> = computed(() => {
+    return (
+      `${environment.tmdb.imageBaseUrl}original${this.images.value()?.[0]?.images?.posters?.[0]?.file_path}` ||
+      '/images/placeholder.png'
+    );
+  });
+  protected readonly fourImages: Signal<string[]> = computed(() => {
+    const posters: string[] = this.images.value()?.map((item) => {
+      if (item.images.posters.length > 0) {
+        return `${environment.tmdb.imageBaseUrl}original${item.images.posters[0].file_path}`;
+      }
+      return '/images/placeholder.png';
+    });
 
-  private readonly mediaMovieService: MediaMovieService = inject(MediaMovieService);
-  private readonly mediaTvSeriesService: MediaTvSeriesService = inject(MediaTvSeriesService);
+    // Ensure we always return an array of 4 images, filling in placeholders as needed.
+    const filledPosters: string[] = posters ? [...posters] : [];
+    while (filledPosters.length < 4) {
+      filledPosters.push('/images/placeholder.png');
+    }
+
+    return filledPosters.slice(0, 4);
+  });
+  protected readonly lastModificationLabel: Signal<string> = computed(() => {
+    const timestamp = this.playlist().lastEditTimestamp;
+    const parsedTime = Date.parse(timestamp);
+
+    // Fallback in case the timestamp is invalid or missing
+    if (isNaN(parsedTime)) {
+      return 'Unknown';
+    }
+
+    const editDate = new Date(parsedTime);
+    const today = new Date();
+
+    editDate.setHours(0, 0, 0, 0);
+    today.setHours(0, 0, 0, 0);
+
+    const diffInMs = today.getTime() - editDate.getTime();
+    const diffInDays = Math.round(diffInMs / (1000 * 60 * 60 * 24));
+
+    if (diffInDays === 0) {
+      return 'Today';
+    }
+
+    if (diffInDays === 1) {
+      return 'Yesterday';
+    }
+
+    if (diffInDays > 1 && diffInDays <= 5) {
+      return `${diffInDays} Days Ago`;
+    }
+
+    return 'Long time ago';
+  });
+  protected readonly itemsCountLabel: Signal<string> = computed(() => {
+    const count: number = this.playlist().items.length;
+
+    if (count === 0) {
+      return 'Empty';
+    }
+
+    if (count > 100) {
+      return '100+ Items';
+    }
+
+    return `${count} Item${count > 1 ? 's' : ''}`;
+  });
+
+  public readonly playlist: InputSignal<OfflinePlaylist> = input.required();
 
   private getMediaImages(trackId: number, mediaType: SearchableMediaType): Observable<ImagesModel> {
     switch (mediaType) {
--- frontend/src/app/playlists/playlists.me.ts
+++ frontend/src/app/playlists/playlists.me.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { UserLocalPlaylistService } from '@shared/services/user/user-local-playlist.service';
+import { OfflinePlaylist } from '@shared/models';
+import { PlaylistsEntryMe } from '@playlists/features/playlists-entry/playlists-entry.me';
 
 @Component({
   selector: 'me-playlists',
-  imports: [],
+  imports: [PlaylistsEntryMe],
   templateUrl: './playlists.me.html',
   styleUrl: './playlists.me.css',
 })
-export class PlaylistsMe {}
+export class PlaylistsMe {
+  private readonly userLocalPlaylistService: UserLocalPlaylistService =
+    inject(UserLocalPlaylistService);
+
+  protected readonly playlists: Signal<OfflinePlaylist[]> = this.userLocalPlaylistService.playlists;
+}
